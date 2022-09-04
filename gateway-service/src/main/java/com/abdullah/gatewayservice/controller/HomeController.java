f7612d4a226a33fe74bb80d34758f5e9b2635478
Abdullah Al Noman <abdullah.noman@advisori.de>
2022-09-04 17:52:00 +0200

    Updated Code

---
+++ gateway-service/src/main/java/com/abdullah/gatewayservice/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.abdullah.gatewayservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        // This is useful for debugging
+        // When having multiple instance of gallery service running at different ports.
+        // We load balance among them, and display which instance received the request.
+        return "Hello from Gateway service";
+    }
+}
